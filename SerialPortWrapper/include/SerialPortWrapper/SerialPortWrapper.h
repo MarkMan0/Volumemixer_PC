6d668a3d0787f5e0a2f847895d85bbf430b72165
MarkMan0 <pal.mark28@gmail.com>
2022-10-09 16:07:25 +0200

    Add library for serial port

---
+++ SerialPortWrapper/include/SerialPortWrapper/SerialPortWrapper.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+#include <Windows.h>
+#include <WinBase.h>
+#include <stdint.h>
+#include <string_view>
+#include <string>
+
+/// @brief Wraps a windows COM port to ease working with it
+class SerialPortWrapper {
+public:
+  SerialPortWrapper(std::wstring_view port, int baud);
+  SerialPortWrapper(int port, int baud);
+
+  SerialPortWrapper(const SerialPortWrapper&) = delete;
+  SerialPortWrapper& operator=(const SerialPortWrapper&) = delete;
+
+  ~SerialPortWrapper();
+
+  void open();
+  void close();
+
+  /// @brief  Check if port is open and ready
+  bool operator()() const;
+
+  int write(uint8_t*, size_t);
+  int read(uint8_t*, size_t);
+  void flush();
+  char get_char();
+  void put_char(char);
+
+
+private:
+  const std::wstring port_name_;
+  const int baud_{};
+  HANDLE com_handle_ = INVALID_HANDLE_VALUE;
+  DCB dcb_old_;
+  DCB dcb_new_;
+  COMMTIMEOUTS timeout_old_;
+  COMMTIMEOUTS timeout_new_;
+};
---
+++ SerialPortWrapper/src/SerialPortWrapper.cpp
@@ -0,0 +1,111 @@
+#include "SerialPortWrapper/SerialPortWrapper.h"
+#include <cstdio>
+
+SerialPortWrapper::SerialPortWrapper(std::wstring_view port, int baud) : port_name_(port), baud_(baud) {
+}
+
+SerialPortWrapper::SerialPortWrapper(int port, int baud) : SerialPortWrapper(L"//.////COM" + std::to_wstring(port), baud) {
+}
+
+void SerialPortWrapper::open() {
+  com_handle_ = CreateFile(port_name_.c_str(), GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, 0, 0);
+  if (com_handle_ == INVALID_HANDLE_VALUE) return;
+
+  GetCommTimeouts(com_handle_, &timeout_old_);
+
+  timeout_new_ = timeout_old_;
+
+  timeout_new_.ReadIntervalTimeout = MAXDWORD;
+  timeout_new_.ReadTotalTimeoutConstant = 0;
+  timeout_new_.ReadTotalTimeoutMultiplier = 0;
+  timeout_new_.WriteTotalTimeoutMultiplier = 0;
+  timeout_new_.WriteTotalTimeoutConstant = 0;
+
+  SetCommTimeouts(com_handle_, &timeout_new_);
+
+  GetCommState(com_handle_, &dcb_old_);
+
+  dcb_new_ = dcb_old_;
+
+  dcb_new_.BaudRate = baud_;
+  dcb_new_.ByteSize = 8;
+  dcb_new_.fParity = 0;
+  dcb_new_.Parity = 0;
+  dcb_new_.StopBits = 0;
+
+  SetCommState(com_handle_, &dcb_new_);
+}
+
+
+SerialPortWrapper::~SerialPortWrapper(void) {
+  close();
+}
+
+
+void SerialPortWrapper::close() {
+  if (com_handle_ == INVALID_HANDLE_VALUE) {
+    return;
+  }
+
+  SetCommTimeouts(com_handle_, &timeout_old_);
+  SetCommState(com_handle_, &dcb_old_);
+
+  CloseHandle(com_handle_);
+
+  com_handle_ = INVALID_HANDLE_VALUE;
+}
+
+int SerialPortWrapper::write(uint8_t* data, size_t sz) {
+  if (com_handle_ == INVALID_HANDLE_VALUE) {
+    return -1;
+  }
+  unsigned long count = 0;
+  WriteFile(com_handle_, data, sz, &count, NULL);
+  return count;
+}
+
+int SerialPortWrapper::read(uint8_t* data, size_t sz) {
+  if (com_handle_ == INVALID_HANDLE_VALUE) {
+    return -1;
+  }
+  unsigned long count = 0;
+  ReadFile(com_handle_, data, sz, &count, NULL);
+  return count;
+}
+
+bool SerialPortWrapper::operator()() const {
+  return com_handle_ != INVALID_HANDLE_VALUE;
+}
+
+void SerialPortWrapper::flush() {
+  if (com_handle_ == INVALID_HANDLE_VALUE) {
+    return;
+  }
+  FlushFileBuffers(com_handle_);
+}
+
+char SerialPortWrapper::get_char() {
+  if (com_handle_ == INVALID_HANDLE_VALUE) {
+    return -1;
+  }
+  unsigned long count;
+  char c;
+  ReadFile(com_handle_, &c, 1, &count, NULL);
+
+  if (count == 1) {
+    return c;
+  }
+
+
+  return -1;
+}
+
+void SerialPortWrapper::put_char(char C) {
+  if (com_handle_ == INVALID_HANDLE_VALUE) {
+    return;
+  }
+
+  unsigned long count;
+
+  WriteFile(com_handle_, &C, 1, &count, NULL);
+}
